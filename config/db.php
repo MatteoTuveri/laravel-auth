196ef839a62a45a1c395a897ae2feca222e5bd63
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2024-01-12 15:47:07 +0100

    initial scaffolding ( db and -rcsm --requests

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php
+    return [
+        'projects' =>[
+            [
+                "title" => "Comics",
+                "description" => "Site to explore the DC Comics catalog of comics",
+                "technologies" => ["Laravel", "php"],
+                "authors" => ["Matteo"],
+            ],
+            [
+                "title" => "Molisana",
+                "description" => "Site to explore the Molisana's pasta catalog",
+                "technologies" => ["Laravel", "php"],
+                "authors" => ["Matteo","Clelia"],
+            ],
+        ]
+    ]
+?>
